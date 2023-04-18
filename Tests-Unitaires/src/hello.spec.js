02cf33d48561b35aaf5b70a2ef77496c6c38c59e
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-04-18 15:11:33 +0200

    Début Mocha

---
+++ Tests-Unitaires/src/hello.spec.js
@@ -0,0 +1,19 @@
+import { it } from 'mocha';
+import { expect } from 'chai';
+import hello from './hello.js';
+
+// it('should return Hello Romain', () => {
+//   const msg = hello('Romain');
+
+//   if (msg !== 'Hello Romain !') {
+//     throw new Error('hello returns ' + msg + ', instead of Hello Romain')
+//   }
+// });
+
+// it('should return Hello Romain', () => {
+//   assert.strictEqual(hello('Romain'), 'Hello Romain !');
+// });
+
+it('should return Hello Romain', () => {
+  expect(hello('Romain')).to.equals('Hello Romain');
+});
