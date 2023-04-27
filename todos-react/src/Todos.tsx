74cab2a339e5a31ec520426f4b8cddb0c600a993
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-04-27 14:26:37 +0200

    Exercice Boucle et Rendu conditionnel

--- todos-react/src/Todos.tsx
+++ todos-react/src/Todos.tsx
@@ -9,18 +9,14 @@ function Todos() {
     { id: 2, title: 'HIJ', completed: true },
     { id: 3, title: 'KLM', completed: false },
   ];
+  const editingId: number | null = 2;
 
-  // Exercice Props
-  // Définir les types Props pour TodoForm
-  // et TodosList de sorte à recevoir les valeurs
-  // si dessus
-  // Dans TodoForm afficher la valeur dans le champs
-  // Dans TodoList passer les valeur à TodoItem (sans faire de boucle pour l'instant) :
-  // <TodoItem item={items[0]} />
-  // <TodoItem item={items[1]} />
-  // <TodoItem item={items[2]} />
-  // Dans TodoItem définir le type Props et afficher les valeurs
-  // au endroit prévu dans le JSX
+  // Exercice Props et Boucle et Rendu conditionnel
+  // 1/ Passer à TodoList la valeur de editingId via les props
+  // 2/ Boucler sur la props items dans TodosList (à la place des 3 TodoItem)
+  // 3/ Ajouter un props isEditing à TodoItem et lui passer true si editingId correspond
+  // à l'item sur lequel vous bouclez (dans TodosList)
+  // 4/ Dans TodoItem afficher un <input /> à la place du <span> si isEditing vaut true
 
   return (
     <div className="Todos">
